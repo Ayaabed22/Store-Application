10f6b4ce342a0e6fb31e312598609f15d841f0a3
Asmaa Karam <asmaakaram793@gmail.com>
2022-11-02 18:18:00 +0200

    MAP-20 create delete icon on favourite fragment to delete item from room database

--- app/src/main/java/com/example/storeapplication/favourite/ui/FavouriteFragment.kt
+++ app/src/main/java/com/example/storeapplication/favourite/ui/FavouriteFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
+import com.example.storeapplication.HomeFragmentDirections
+import com.example.storeapplication.R
 import com.example.storeapplication.databinding.FragmentFavouriteBinding
 import com.example.storeapplication.favourite.data.FavouriteDatabase
 import com.example.storeapplication.utils.Const.Companion.favouriteDao
@@ -43,6 +47,7 @@ class FavouriteFragment : Fragment() ,ItemClick{
     }
 
     override fun productClickListener(id: Int) {
-        TODO("Not yet implemented")
+        val action= FavouriteFragmentDirections.actionFavouriteFragmentToDeatilesFragment(id)
+        findNavController().navigate(action)
     }
 }
